d8930fa244942a6042a9d034e1530b2d1710f027
HenriqueSerres <henriqueserres@gmail.com>
2022-01-03 13:51:18 -0300

    exercicios 1 ao 9

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div>
+        <div data-testid="solar-system" />
+        <Title headline="Planetas" />
+        {planets.map((planet) => (
+          <div key={ planet.name }>
+            <PlanetCard planetName={ planet.name } planetImage={ planet.image } />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
